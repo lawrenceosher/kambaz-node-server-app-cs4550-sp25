05f4104748599f59c4af296704be9534180b0076
Lawrence Osher <lao2205@gmail.com>
2025-03-24 18:46:37 -0400

    Supporting multiple user sessions

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,6 +1,5 @@
 import * as dao from "./dao.js";
 
-let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = (req, res) => {};
   const deleteUser = (req, res) => {};
@@ -11,7 +10,8 @@ export default function UserRoutes(app) {
     const userId = req.params.userId;
     const userUpdates = req.body;
     dao.updateUser(userId, userUpdates);
-    currentUser = dao.findUserById(userId);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
@@ -21,23 +21,33 @@ export default function UserRoutes(app) {
       res.status(400).json({ message: "Username already in use" });
       return;
     }
-    currentUser = dao.createUser(req.body);
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
   const signin = (req, res) => {
     const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
   };
 
   const signout = (req, res) => {
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
 
   const profile = async (req, res) => {
-    res.json(currentUser);
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
   };
 
   app.post("/api/users", createUser);
