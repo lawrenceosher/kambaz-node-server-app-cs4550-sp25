0ce9acb4edd782306f92a64c66b04bab882bb902
Lawrence Osher <lao2205@gmail.com>
2025-03-26 16:12:16 -0400

    fixed unenroll from courses

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -8,8 +8,11 @@ export function enrollUserInCourse(userId, courseId) {
 }
 
 export function unenrollUserFromCourse(userId, courseId) {
+  const enrollment = Database.enrollments.find(e => e.course === courseId && e.user === userId);
+  if (!enrollment) return;
+
   Database.enrollments = Database.enrollments.filter(
-    (e) => e.course !== courseId && e.user !== userId
+    (e) => e._id !== enrollment._id
   );
 }
 
