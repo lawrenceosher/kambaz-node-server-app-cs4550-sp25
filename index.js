8e7e8fc0ce870050a158d7fabefa5af3585ae16f
Lawrence Osher <lao2205@gmail.com>
2025-04-05 12:38:26 -0400

    installing and connecting to a mongodb database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +10,8 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 
+const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
