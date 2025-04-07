b3a667c915649f1b2adcda0cdc79c61c3d1046db
Lawrence Osher <lao2205@gmail.com>
2025-04-07 18:08:32 -0400

    filtering out null users for finding users by course

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,20 +1,21 @@
 import model from "./model.js";
 
 export async function findCoursesForUser(userId) {
- const enrollments = await model.find({ user: userId }).populate("course");
- return enrollments.map((enrollment) => enrollment.course);
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
 }
 
 export async function findUsersForCourse(courseId) {
- const enrollments = await model.find({ course: courseId }).populate("user");
- return enrollments.map((enrollment) => enrollment.user);
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments
+    .filter((e) => e.user !== null)
+    .map((enrollment) => enrollment.user);
 }
 
 export async function enrollUserInCourse(user, course) {
- return await model.create({ user, course, _id: `${user}-${course}` });
+  return await model.create({ user, course, _id: `${user}-${course}` });
 }
 
 export async function unenrollUserFromCourse(user, course) {
- return await model.deleteOne({ user, course });
+  return await model.deleteOne({ user, course });
 }
-
